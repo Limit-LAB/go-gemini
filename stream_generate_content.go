6fa735d047b921f0ef5a594eb840ac7e3b137658
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-12-15 02:40:53 +0000

    feat: streamer

--- stream_generate_content.go
+++ stream_generate_content.go
@@ -1,18 +1,80 @@
 package gemini
 
-import "github.com/Limit-LAB/go-gemini/models"
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"github.com/Limit-LAB/go-gemini/models"
+	"io"
+	"strings"
+)
 
-func (c *Client) GenerateContentStream(model models.GeminiModel, req models.GenerateContentRequest) (models.GenerateContentResponse, error) {
+func (c *Client) GenerateContentStream(model models.GeminiModel, req models.GenerateContentRequest) (*GenerateContentStreamer, error) {
 	for _, content := range req.Contents {
 		err := validateGenerateContentRequest(model, content)
 		if err != nil {
-			return models.GenerateContentResponse{}, err
+			return nil, err
 		}
 	}
 	url := c.url(string(model), "streamGenerateContent")
-	return unjson[models.GenerateContentResponse](c.post(url, req))
-	// TODO: implement stream
+	httpReq, err := c.newReq("POST", url, req)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.hc.Do(httpReq)
+	if err != nil {
+		return nil, err
+	}
+	return newStreamScanner(resp.Body), nil
 }
 
-type GenerateContentStream struct {
+type GenerateContentStreamer struct {
+	buf *bufio.Scanner
+	raw io.ReadCloser
+}
+
+func (r *GenerateContentStreamer) Close() error {
+	return r.raw.Close()
+}
+
+func (r *GenerateContentStreamer) Receive() (models.GenerateContentResponse, error) {
+	if !r.buf.Scan() {
+		err := r.buf.Err()
+		if err == nil {
+			err = io.EOF
+		}
+		return models.GenerateContentResponse{}, err
+	}
+	txt := r.buf.Text()
+	// remove head '[' and tail ']'
+	txt = strings.TrimLeft(txt, "[,\r\n")
+	txt = strings.TrimRight(txt, "],\r\n")
+	var res models.GenerateContentResponse
+	err := json.Unmarshal([]byte(txt), &res)
+	return res, err
+}
+
+func newStreamScanner(eventStream io.ReadCloser) *GenerateContentStreamer {
+	scanner := bufio.NewScanner(eventStream)
+
+	scanner.Buffer(make([]byte, 4096), 4096)
+
+	split := func(data []byte, atEOF bool) (int, []byte, error) {
+		if i := bytes.Index(data, []byte("}\n,\r\n")); i >= 0 {
+			return i + 5, data[0 : i+1], nil
+		}
+		if atEOF && len(data) == 0 {
+			return 0, nil, nil
+		}
+		if atEOF {
+			return len(data), data, nil
+		}
+		return 0, nil, nil
+	}
+	scanner.Split(split)
+
+	return &GenerateContentStreamer{
+		buf: scanner,
+		raw: eventStream,
+	}
 }
---
+++ stream_generate_content_test.go
@@ -0,0 +1,36 @@
+package gemini_test
+
+import (
+	"fmt"
+	"github.com/Limit-LAB/go-gemini"
+	"github.com/Limit-LAB/go-gemini/models"
+	"github.com/joho/godotenv"
+	"os"
+	"testing"
+)
+
+func TestGeminiStreamGenerateContent(t *testing.T) {
+	godotenv.Load()
+	key := os.Getenv("GEMINI")
+	req := models.NewGenerateContentRequest(nil).
+		AppendContent(
+			models.RoleUser,
+			models.NewParts(nil).
+				AppendPart(models.NewTextPart("Hi! Use 20 words to describe yourself.")),
+		)
+	cli := gemini.NewClient(key)
+	gcs, err := cli.GenerateContentStream(models.GeminiPro, req)
+	if err != nil {
+		panic(err)
+	}
+	defer gcs.Close()
+	for {
+		content, err := gcs.Receive()
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Println("==============LOOP==============")
+		jsonPrint(content)
+	}
+}
