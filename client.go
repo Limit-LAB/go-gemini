47997f89204e49067c2d26558989942c36277015
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-12-17 02:13:34 +0000

    feat: support VertexAI

--- client.go
+++ client.go
@@ -1,6 +1,9 @@
 package gemini
 
-import "net/http"
+import (
+	"github.com/Limit-LAB/go-gemini/models"
+	"net/http"
+)
 
 type AuthBy string
 
@@ -32,5 +35,18 @@ func (c *Client) SetBaseUrl(url string) *Client {
 
 func (c *Client) SetAuthWay(auth AuthBy) *Client {
 	c.auth = auth
+	return c
+}
+
+func (c *Client) VertexAI(region models.VertexRegion, projId string) *Client {
+	rg := string(region)
+	c.auth = AuthByHttpHeader
+	c.baseUrl = "https://" + rg + "-aiplatform.googleapis.com/v1/projects/" + projId + "/locations/" + rg + "/publishers/google/models/"
+	return c
+}
+
+func (c *Client) AIStudio() *Client {
+	c.auth = AuthByUrlQuery
+	c.baseUrl = "https://generativelanguage.googleapis.com/v1beta/models/"
 	return c
 }
---
+++ models/vertexRegion.go
@@ -0,0 +1,25 @@
+package models
+
+type VertexRegion string
+
+const (
+	VertexRegion_US_Iowa              VertexRegion = "us-central1"
+	VertexRegion_US_Las_Vegas         VertexRegion = "us-west4"
+	VertexRegion_CA_Montreal          VertexRegion = "northamerica-northeast1"
+	VertexRegion_US_Northern_Virginia VertexRegion = "us-east4"
+	VertexRegion_US_Oregon            VertexRegion = "us-west1"
+	VertexRegion_KR_Seoul             VertexRegion = "asia-northeast3"
+	VertexRegion_SG_Singapore         VertexRegion = "asia-southeast1"
+	VertexRegion_JP_Tokyo             VertexRegion = "asia-northeast1"
+)
+
+const (
+	VertexRegion_US_Central_1             VertexRegion = "us-central1"
+	VertexRegion_US_West_4                VertexRegion = "us-west4"
+	VertexRegion_NorthAmerica_NorthWast_1 VertexRegion = "northamerica-northeast1"
+	VertexRegion_US_East_4                VertexRegion = "us-east4"
+	VertexRegion_US_West_1                VertexRegion = "us-west1"
+	VertexRegion_Asia_NorthEast_3         VertexRegion = "asia-northeast3"
+	VertexRegion_Asia_SouthEast_1         VertexRegion = "asia-southeast1"
+	VertexRegion_Asia_NorthEast_1         VertexRegion = "asia-northeast1"
+)
